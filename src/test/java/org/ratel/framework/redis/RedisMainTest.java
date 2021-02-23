0121228b2111ce52e2be5efc4d759874c1791bed
dongchuang <dongsiqi@bonc.com.cn>
2021-02-23 17:36:43 +0800

    初始化

--- src/test/java/org/ratel/framework/redis/RedisMainTest.java
+++ src/test/java/org/ratel/framework/redis/RedisMainTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -28,18 +29,23 @@ public class RedisMainTest {
         while (it.hasNext()) {
             UserEntity user2 = it.next();
             System.out.println("user:" + user2.getKey() + "," + user2.getUserName() + "," + user2.getUserSex());
+            System.out.println("Create-Time:" + user2.getCreateTime());
         }
     }
 
     @Test
     public void testSerializable() {
-        UserEntity user = new UserEntity();
-        user.setId(1L);
-        user.setUserName("朝雾轻寒");
-        user.setUserSex("男");
-        strRedisTemplate.set(user);
-        UserEntity user2 = (UserEntity) strRedisTemplate.get(user.getKey());
-        System.out.println("user:" + user2.getId() + "," + user2.getUserName() + "," + user2.getUserSex());
+        for (int i = 0; i < 100; i++) {
+            UserEntity user = new UserEntity();
+            user.setId(1L);
+            user.setUserName("朝雾轻寒" + i);
+            user.setUserSex("男");
+            user.setCreateTime(new Date());
+            strRedisTemplate.set(user);
+            UserEntity user2 = (UserEntity) strRedisTemplate.get(user.getKey());
+            System.out.println("user:" + user2.getId() + "," + user2.getUserName() + "," + user2.getUserSex());
+            System.out.println("Create-Time:" + user2.getCreateTime());
+        }
     }
 
 }
